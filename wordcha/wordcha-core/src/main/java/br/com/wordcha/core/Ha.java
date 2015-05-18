3a65da39dfe10f6ed0f7c0a7aaca725ce829e902
alexandredeassis <aaa>
2015-05-18 17:07:44 -0300

    pra 20

--- wordcha/wordcha-core/src/main/java/br/com/wordcha/core/Ha.java
+++ wordcha/wordcha-core/src/main/java/br/com/wordcha/core/Ha.java
@@ -3,7 +3,7 @@ package br.com.wordcha.core;
 public class Ha {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++)
+		for (int i = 0; i < 20; i++)
 			System.err.println("ha");
 	}
 }
