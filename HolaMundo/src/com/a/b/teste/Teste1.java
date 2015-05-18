0d31e05bdd8149c68db6328f508b1df84e73193b
alexandredeassis <alex.alex01>
2015-05-18 12:19:41 -0300

    asdasd

--- HolaMundo/src/com/a/b/teste/Teste1.java
+++ HolaMundo/src/com/a/b/teste/Teste1.java
@@ -3,7 +3,11 @@ package com.a.b.teste;
 public class Teste1 {
 
 	public static void main(String[] args) {
-		System.out.println("foi..");
+		for (int i = 0; i < 10; i++) {
+
+			System.out.println("foi..");
+		}
+
 	}
-	
+
 }
