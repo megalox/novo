3ce82afc96c2c81fd35358cb343f797eb4cdaf2d
alexandredeassis <alex.alex01>
2015-05-19 10:27:58 -0300

    teste

--- wordcha/wordcha-core/src/main/java/br/com/wordcha/core/User.java
+++ wordcha/wordcha-core/src/main/java/br/com/wordcha/core/User.java
@@ -1,6 +1,20 @@
 package br.com.wordcha.core;
 
-
 public class User {
+
+	private String name;
+
+	public User(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
